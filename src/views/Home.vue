82e041c0945a3263cde3221157435ffca88aa14b
Eduardo Alves <eduardomma99@gmail.com>
2021-10-21 12:06:02 -0300

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,18 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Galeria de Fotos</ion-title>
       </ion-toolbar>
     </ion-header>
     
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
+          <ion-title size="large">Tab 2</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+
+      <ExploreContainer name="Tab 2 page" />
     </ion-content>
   </ion-page>
 </template>
@@ -24,6 +21,7 @@
 <script lang="ts">
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
 import { defineComponent } from 'vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
 
 export default defineComponent({
   name: 'Home',
